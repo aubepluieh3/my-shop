383591d63d1d403202dcbbeb032684cb889e882b
SOO <66086544+aubepluieh3@users.noreply.github.com>
2025-11-22 12:24:59 +0900

    feat: implement state persistence for favorite product list

--- src/store/useFavoriteStore.ts
+++ src/store/useFavoriteStore.ts
@@ -1,19 +1,23 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface FavoriteState {
     favorites: string[];
     toggleFavorite: (id: string) => void;
 }
 
-export const useFavoriteStore = create<FavoriteState>((set, get) => ({
-    favorites: [],
-    toggleFavorite: (id) => {
-        const { favorites } = get();
-        if (favorites.includes(id)) {
-            set({ favorites: favorites.filter((fid) => fid !== id)});
-        } else {
-            set({ favorites: [...favorites, id]})
-        }
-    }}
+export const useFavoriteStore = create(
+    persist<FavoriteState>((set, get) => ({
+        favorites: [],
+        toggleFavorite: (id) => {
+            const { favorites } = get();
+            if (favorites.includes(id)) {
+                set({ favorites: favorites.filter((fid) => fid !== id)});
+            } else {
+                set({ favorites: [...favorites, id]})
+            }
+        }}),
+    {
+        name: 'favorite-storage'
+    })
 )
-);
