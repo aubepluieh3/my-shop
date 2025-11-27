6ef7c0d1dc5826bd0fdddfe959ba363a5c1e73eb
SOO <66086544+aubepluieh3@users.noreply.github.com>
2025-11-27 22:59:19 +0900

    feat: add useAuthStore

---
+++ src/store/useAuthStore.ts
@@ -0,0 +1,34 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface User {
+    id: string,
+    email: string,
+    name: string,
+    role: "user" | "admin",
+    profileImage?: string;
+    level?: string;
+}
+
+interface AuthState {
+    user: User | null,
+    token: string | null,
+    setUser: (user:User, token: string) => void;
+    getUser: () => User | null;
+    logout: () => void;
+}
+
+export const useAuthStore = create(
+  persist<AuthState>(
+    (set, get) => ({
+      user: null,
+      token: null,
+      getUser: () => get().user,
+      setUser: (user, token) => set({ user, token }),
+      logout: () => set({ user: null, token: null }),
+    }),
+    {
+      name: "auth-storage",
+    }
+  )
+);
